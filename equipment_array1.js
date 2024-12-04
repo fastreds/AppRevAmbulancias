7837389d732e14c2a37471e5926006dea82e9cab
fastreds <142920045+fastreds@users.noreply.github.com>
2024-12-03 18:07:39 -0600

    Crear archivo

---
+++ equipment_array1.js
@@ -0,0 +1,6 @@
+
+                        const equipment = [
+                            { id: 1, name: "Monitor", quantity: 2 },
+                            { id: 2, name: "Oxygen Tank", quantity: 4 }
+                        ];
+                    
