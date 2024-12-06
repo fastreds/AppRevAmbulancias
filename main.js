2198467252ed287b482a336d1968bb5203102c96
fastreds <fastreds@gmail.com>
2024-12-06 09:16:04 -0600

    Actulizacion mantenimiento

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
-
 var jsonModelo = "equipment_array.json";
 
 // Función para cargar y leer el archivo JSON de manera síncrona
 function cargarItemsSinc() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', jsonModelo, false);  // `false` hace que la solicitud sea síncrona
-    xhr.setRequestHeader('Content-Type', 'application/json');
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", jsonModelo, false); // `false` hace que la solicitud sea síncrona
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-    try {
-        xhr.send();  // Envía la solicitud
-        if (xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);  // Procesar la respuesta JSON
-            console.log("Items generados desde JSON:", data);
-            return data;
-        } else {
-            console.error('Error al cargar el archivo JSON:', xhr.statusText);
-        }
-    } catch (error) {
-        console.error('Error al cargar el archivo JSON:', error);
+  try {
+    xhr.send(); // Envía la solicitud
+    if (xhr.status === 200) {
+      const data = JSON.parse(xhr.responseText); // Procesar la respuesta JSON
+      //console.log("Items generados desde JSON:", data);
+      return data;
+    } else {
+      console.error("Error al cargar el archivo JSON:", xhr.statusText);
     }
+  } catch (error) {
+    console.error("Error al cargar el archivo JSON:", error);
+  }
 }
 
 // Llamar a la función para cargar los items
 function obtenerItemsSinc() {
-    const items = cargarItemsSinc();  // Llama a la función síncrona
-    console.log("Items obtenidos:", items);
-    return items;  // Retorna los items cargados
+  const items = cargarItemsSinc(); // Llama a la función síncrona
+  //console.log("Items obtenidos:", items);
+  return items; // Retorna los items cargados
 }
 
 // Obtener los items
-const items = obtenerItemsSinc();  // Aquí obtienes los items de manera síncrona
+const itemsModelo = obtenerItemsSinc(); // Aquí obtienes los items de manera síncrona
 
-
-
+//gestiona los  id o numeros de cadena a entero
+items = itemsModelo.map((item) => ({
+  ...item,
+  id: parseInt(item.id, 10),
+  CantidadSugerida: parseInt(item.CantidadSugerida, 10),
+  CantidadMinina: parseInt(item.CantidadMinina, 10),
+  Opcionales:
+    typeof item.Opcionales === "string"
+      ? JSON.parse(item.Opcionales || "{}")
+      : item.Opcionales,
+}));
 
 ////////////////////////////////   toastify     //////////////////////////////////
 
@@ -46,7 +54,6 @@ link.href = "https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css";
 // Agregar el elemento <link> al <head> del documento
 document.head.appendChild(link);
 
-
 //   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
 //toastify
 const script = document.createElement("script");
@@ -57,26 +64,24 @@ script.src = "https://cdn.jsdelivr.net/npm/toastify-js";
 document.head.appendChild(script);
 
 function notificar(textRecived) {
-    Toastify({
-      text: textRecived,
-      duration: 2000, // Duración en milisegundos
-      gravity: "top", // top o bottom
-      position: "left", // right o left
-      backgroundColor: "linear-gradient(to right, #0072CE, #ccc)",
-      stopOnFocus: true // Detiene la notificación si el usuario pasa el mouse por encima
-    }).showToast();
-  }
+  Toastify({
+    text: textRecived,
+    duration: 2000, // Duración en milisegundos
+    gravity: "top", // top o bottom
+    position: "left", // right o left
+    backgroundColor: "linear-gradient(to right, #0072CE, #ccc)",
+    stopOnFocus: true, // Detiene la notificación si el usuario pasa el mouse por encima
+  }).showToast();
+}
 
-  //////////////////    fin toastify /////////////////////////////
-
-
+//////////////////    fin toastify /////////////////////////////
 
 ///////////////// enviar correo generico ////////////////////
 
-const scriptURLCorreos = "https://script.google.com/macros/s/AKfycbw6I6TcDab7w3qiXrGPRpr4FbnUxdK6kqPsIWVi6cohcwBPlC02NVLH1TVa6ylp80UJzA/exec"; 
+const scriptURLCorreos =
+  "https://script.google.com/macros/s/AKfycbw6I6TcDab7w3qiXrGPRpr4FbnUxdK6kqPsIWVi6cohcwBPlC02NVLH1TVa6ylp80UJzA/exec";
 
-
- function sendEmail(email, subject, message) {
+function sendEmail(email, subject, message) {
   const data = {
     email: email,
     subject: subject,
@@ -84,15 +89,15 @@ const scriptURLCorreos = "https://script.google.com/macros/s/AKfycbw6I6TcDab7w3q
   };
 
   try {
-    const response =  fetch(scriptURLCorreos, {
-      method: 'POST',
-      mode: 'no-cors',
+    const response = fetch(scriptURLCorreos, {
+      method: "POST",
+      mode: "no-cors",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     });
-    const result =  response.json();
+    const result = response.json();
     if (result.success) {
       alert("Correo enviado con éxito.");
     } else {
@@ -104,24 +109,6 @@ const scriptURLCorreos = "https://script.google.com/macros/s/AKfycbw6I6TcDab7w3q
 }
 
 //sendEmail("fastreds@gmail.com", "test", "enviar el correo") ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /*
   ///////////////////////////////////bootstrap ///////////////////////////////////////
